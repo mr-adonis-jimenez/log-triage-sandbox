a3a33c15ca16ca6d0fccce3e2133337264700614
Adonis Jimenez <Adonis.Jimenez@student.ctuonline.edu>
2026-01-17 05:35:57 -0500

    Create App component with LogTriageSandbox
    Main application entry point that imports and renders the LogTriageSandbox component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogTriageSandbox from './components/LogTriageSandbox';
+import './styles/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LogTriageSandbox />
+    </div>
+  );
+}
+
+export default App;
