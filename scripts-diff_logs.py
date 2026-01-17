54b532b275c5e77a813c90efa811c13732cc254a
Adonis Jimenez <Adonis.Jimenez@student.ctuonline.edu>
2026-01-17 13:32:31 -0500

    Delete scripts-diff_logs.py

--- scripts-diff_logs.py
+++
@@ -1,8 +0,0 @@
-import difflib
-from pathlib import Path
-
-a = Path("logs/parsed/passing.log").read_text().splitlines()
-b = Path("logs/parsed/failing.log").read_text().splitlines()
-
-for line in difflib.unified_diff(a, b, lineterm=""):
-    print(line)
